c10757cfbd6ab3d70d568cc7d755848076b99eae
QuarterSalt <Leifpeterolson@gmail.com>
2024-12-07 01:19:00 -0500

    Initial commit

---
+++ src/main/java/net/quartersalt/shit/SHITClient.java
@@ -0,0 +1,10 @@
+package net.quartersalt.shit;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SHITClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/quartersalt/shit/SlightlyHelpfulImprovisedTweaks.java
@@ -0,0 +1,15 @@
+package net.quartersalt.shit;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SlightlyHelpfulImprovisedTweaks implements ModInitializer {
+	public static final String MOD_ID = "shit";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
